11720f9e01a83e8fab03c01410e72a4e9b5021cf
Bycor Sanchez <bycor1989@gmail.com>
2017-11-28 10:53:24 +0000

    First JavaScript challenges

---
+++ javascript/roadtrip/level 1.2.js
@@ -0,0 +1,38 @@
+//Task1
+var age = 18;
+
+//Task2
+var ageIn5Years = age + 5;
+
+//Task3
+var firstName = "Bycor";
+
+//Task4
+var lastName = "Sanchez";
+
+//Task5
+var fullName = firstName + " " + lastName;
+
+//Task6
+var index14 = "What can we do? Keep going!".charAt(14);
+
+//Task7
+var sentence = "This variable is already assigned. Find its length without manually counting the characters and spaces";
+sentence.length;
+
+//Task8
+var numPassengers = 0;
+numPassengers += 3;
+
+//Task9
+var countMessage = "Attention: There are now "+ numPassengers + " passengers on the train!";
+
+//Task10
+var trainCapacity = 60;
+var occupancy = numPassengers + " passengers present, with " + (trainCapacity  - numPassengers) + " seats remaining.";
+
+//Task11
+var twenty20 = 3 + 10 + 7;
+
+//Task12
+twenty20 = "Never look back".charAt(0) + "Adventure is out there".charAt(3) + "Just keep swimming".charAt(14) + "To infinity... and beyond!".charAt(1);
