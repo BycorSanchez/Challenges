d024f7f9347c28f9780d39ef51701bfd47643fc9
Bycor Sánchez <bycor1989@gmail.com>
2017-12-29 19:24:34 +0000

    Add back to top link

--- webs/13-12272017/main.js
+++ webs/13-12272017/main.js
@@ -1,44 +1,47 @@
 $(document).ready(function() {
 
-    function updateActive() {
-        const activeLink = 'active-link';
-        const position = $(this).scrollTop();
+    const activeClass = 'active-link';
+    const menuVisibleClass = 'menu-visible';
+    
 
+    function smoothScroll(target) {
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 'linear');
+    };
+
+    function toggleMenu() {
+        $('body').toggleClass(menuVisibleClass);
+    };
+
+    function updateActive(position) {
         $('#nav a').each(function() {
             let link = $(this);
             let reference = $(link.attr("href"));
 
             let top = reference.position().top - 1;
-            let bottom = top + reference.height() + 1;
+            let bottom = top + reference.outerHeight();
 
             if (top <= position && bottom > position) {
-                $('#nav').find('a').removeClass(activeLink);
-                link.addClass(activeLink);
+                $('#nav').find('a').removeClass(activeClass);
+                link.addClass(activeClass);
             } else {
-                link.removeClass(activeLink);
+                link.removeClass(activeClass);
             }
         });
     };
 
-    function toggleMenu() {
-        $('body').toggleClass('menu-visible');
-    };
-
-    function smoothScroll(target) {
-        if (target === '#') return;
-
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 450, 'linear');
-    };
-
-
     $('#menu header').on('click', 'a', function(event) {
         event.preventDefault();
-        smoothScroll($(this).attr('href'));
+
+        const reference = $(this).attr('href');
+        if (reference !== '#') smoothScroll(reference);
     });
 
     $('#menu-icon').click(toggleMenu);
 
-    $(document).on("scroll", updateActive);
+    $(document).on("scroll", function() {
+        let position = $(this).scrollTop();
+        updateActive(position);
+    });
 });
