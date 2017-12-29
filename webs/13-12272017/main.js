2ed432004ab912d440e30a586fbeaac3f8b44b66
Bycor Sánchez <bycor1989@gmail.com>
2017-12-29 11:50:21 +0000

    Add link scroll & menu display transitions. Remove horizontal scroll in mobile.

--- webs/13-12272017/main.js
+++ webs/13-12272017/main.js
@@ -6,14 +6,28 @@ function updateActive(current) {
     current.addClass(active);
 }
 
+function smoothScroll(target) {
+    if (target === '#') return;
+
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 450, 'linear');
+}
+
 function toggleMenu() {
     const visible = 'menu-visible';
     $('body').toggleClass(visible);
 }
 
-$(function onReady() {
-    $('#nav').on('click', 'a', function() {
+/* TODO: Change active nav link on scroll */
+/* TODO: Add back to top icon */
+
+$(document).ready(function() {
+    $('#menu header').on('click', 'a', function(event) {
+        event.preventDefault();
+
         updateActive($(this));
+        smoothScroll($(this).attr('href'));
     });
 
     $('#menu-icon').click(function() {
