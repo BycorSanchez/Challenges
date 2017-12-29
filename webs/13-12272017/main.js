c7265f29f39b85e8068a98ee2f4b0d793e0c52f0
Bycor Sánchez <bycor1989@gmail.com>
2017-12-29 14:02:46 +0000

    Add active link on scroll

--- webs/13-12272017/main.js
+++ webs/13-12272017/main.js
@@ -1,36 +1,44 @@
-function updateActive(current) {
-    const active = 'active-link';
-
-    let links = $('#nav').find('a');
-    links.removeClass(active);
-    current.addClass(active);
-}
-
-function smoothScroll(target) {
-    if (target === '#') return;
-
-    $('html, body').animate({
-        scrollTop: $(target).offset().top
-    }, 450, 'linear');
-}
-
-function toggleMenu() {
-    const visible = 'menu-visible';
-    $('body').toggleClass(visible);
-}
-
-/* TODO: Change active nav link on scroll */
-/* TODO: Add back to top icon */
-
 $(document).ready(function() {
+
+    function updateActive() {
+        const activeLink = 'active-link';
+        const position = $(this).scrollTop();
+
+        $('#nav a').each(function() {
+            let link = $(this);
+            let reference = $(link.attr("href"));
+
+            let top = reference.position().top - 1;
+            let bottom = top + reference.height() + 1;
+
+            if (top <= position && bottom > position) {
+                $('#nav').find('a').removeClass(activeLink);
+                link.addClass(activeLink);
+            } else {
+                link.removeClass(activeLink);
+            }
+        });
+    };
+
+    function toggleMenu() {
+        $('body').toggleClass('menu-visible');
+    };
+
+    function smoothScroll(target) {
+        if (target === '#') return;
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 450, 'linear');
+    };
+
+
     $('#menu header').on('click', 'a', function(event) {
         event.preventDefault();
-
-        updateActive($(this));
         smoothScroll($(this).attr('href'));
     });
 
-    $('#menu-icon').click(function() {
-        toggleMenu();
-    });
+    $('#menu-icon').click(toggleMenu);
+
+    $(document).on("scroll", updateActive);
 });
